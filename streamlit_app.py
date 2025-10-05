c0e70549d191be048499ea35f9fb75ebf353b74a
Kuldeep Nagar <57064666+Ringo1506@users.noreply.github.com>
2025-10-05 18:41:05 +0530

    Update streamlit_app.py
    arrange the code in proper format and formatted code

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen+' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        df_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
 
     # st.write(ingredients_string)
     #create sql queries to write data to snowflake
@@ -48,8 +50,6 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-df_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width = True)
+
 
 
